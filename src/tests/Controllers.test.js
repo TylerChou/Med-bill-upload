64f88653b7a36860818f236415c9d6425e1a509f
tyler_chou <tylerhouse999@gmail.com>
2023-02-15 18:09:50 -0800

    update test

--- src/tests/Controllers.test.js
+++ src/tests/Controllers.test.js
@@ -3,21 +3,18 @@ const request = require('supertest');
 
 const app = require('../server.js');
 const bill = require("../models/bill");
-const { describe } = require('yargs');
 
 const test_bill = new bill ("Tyler", "1065 Kings Court", "John Hospital" , "2/14/2023", 1000);
 const test_wrong_bill = new bill();
+
+describe(
+    "Test bill controller GET/POST",
+    ()=>{
 
 it("GET empty /api/bills", async()=>{
     const response = await request(app).get('/api/bills');
     expect(response.statusCode).toEqual(200);
     expect(response.body).toEqual([]);
-});
-
-it("GET incorrect link", async()=>{
-    const response = await request(app).get('/incorrect_link');
-    expect(response.statusCode).toEqual(404);
-    expect(response.body).toEqual({ message: 'Route Not Found, please navigate to /api/bills' });
 });
 
 it("POST /api/bills incorrect bill", async()=>{
@@ -40,3 +37,12 @@ it("GET 1 Object", async()=>{
     expect(response.statusCode).toEqual(200);
     expect(response.body).toEqual([test_bill]);
 });
+
+})
+describe("Test invalid link",()=>{
+    it("GET incorrect link", async()=>{
+        const response = await request(app).get('/incorrect_link');
+        expect(response.statusCode).toEqual(404);
+        expect(response.body).toEqual({ message: 'Route Not Found, please navigate to /api/bills' });
+    });
+})
